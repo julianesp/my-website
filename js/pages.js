87ae3f8c35ece36ec33d2b8e80e7e131c234e6fd
Julián España <julii1295@gmail.com>
2022-06-20 10:10:48 -0500

    Agregando imágenes y dirección a mis proyectos

--- js/pages.js
+++ js/pages.js
@@ -26,3 +26,11 @@ function registro(params) {
 function tiendas(params) {
     location.href = '../html/tiendas.html'
 }
+
+function cirp(params) {
+    location.href = 'https://julianesp.github.io/awaspa/'
+}
+
+function awaspa(params) {
+    location.href = 'https://julianesp.github.io/cirp/'
+}
